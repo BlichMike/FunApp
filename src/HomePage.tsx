d71572285e94f159cfd7a5fff2e539343e77d07a
Blitz <blich.mike@gmail.com>
2023-07-17 08:59:11 +0300

    asd

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,6 +2,8 @@ import { Typography } from '@mui/material';
 
 export const HomePage: React.FC = () => {
   return (
-    <Typography variant="h4">Welcome to Mamush app, enjoy *____*</Typography>
+    <Typography variant="h4" textAlign={'center'}>
+      Welcome to Mamush app, enjoy *____*
+    </Typography>
   );
 };

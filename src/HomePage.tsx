027198570dab2744bc09f06506217a5dd4b14760
Blitz <blich.mike@gmail.com>
2023-07-17 08:42:35 +0300

    small text fixes

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,6 +2,6 @@ import { Typography } from '@mui/material';
 
 export const HomePage: React.FC = () => {
   return (
-    <Typography variant="h4">Wellcome to our app, enjoy *____*</Typography>
+    <Typography variant="h4">Welcome to Mamush app, enjoy *____*</Typography>
   );
 };
